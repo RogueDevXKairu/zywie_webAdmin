6144905c4a081dca546e215e9bfe63b64efe53d8
famasboy88 <famasboy88@gmail.com>
2018-01-27 16:58:44 +0800

    Added working firebase notification

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,7 +3,6 @@ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
-
 var config = {
     apiKey: "AIzaSyCCluMaWBRVBDbhRNrOgFScGBcQLcsqBus",
     authDomain: "zywie-2b7c2.firebaseapp.com",
--- public/assets/js/notification.js
+++ public/assets/js/notification.js
@@ -4,11 +4,42 @@ firebase.auth().onAuthStateChanged(function(user) {
         window.location="/login.html";
     }
 });
-
-const FIREBASE_MESSAGING = firebase.messaging();
+document.getElementById('loadingSpiner').style.display = 'none';
 
 function pushNotification() {
     var title = document.getElementById("notif-title");
     var description = document.getElementById("notif-description");
-
+    if(title.value!="" && description.value!=""){
+        document.getElementById('loadingSpiner').style.display = 'block';
+        var json = {
+            "to": "/topics/logged_users",
+            "notification": {
+              "title": title.value,
+              "body": description.value
+            }
+          };
+       
+          $.ajax({
+           url: 'https://fcm.googleapis.com/fcm/send',
+           type: "POST",
+           processData : false,
+           beforeSend: function (xhr) {
+             xhr.setRequestHeader('Content-Type', 'application/json');
+             xhr.setRequestHeader('Authorization', 'key=' + 'AIzaSyATWCd5Xv6LmePoQI_yiPco_pzDIdEuz4A');
+           },
+           data: JSON.stringify(json),
+           success: function () {
+             alert("Sent notifications to all devices!");
+             title.value ="";
+            description.value ="";
+           },
+           error: function(error) {
+            alert("Error Notifications to Users");
+           }
+         });
+         document.getElementById('loadingSpiner').style.display = 'none';
+    }else{
+        alert("Fill out all fields");
+    }
+    
 }
